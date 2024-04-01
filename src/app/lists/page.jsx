fb6ba295165121281764289c06187168fefe1463
Nithin <nithinkatkam504106@gmail.com>
2024-04-01 20:50:25 +0530

    listing courses

---
+++ src/app/lists/page.jsx
@@ -0,0 +1,34 @@
+'use client'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Card from './card';
+
+const Page = () => {
+  const [apiData, setApiData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('https://testnets.tableland.network/api/v1/query?statement=select%20%2A%20from%20flbLearn_314159_790');
+        setApiData(response.data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div className="container mx-auto">
+      <h1 className="text-3xl font-semibold mb-8">Data from API</h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-9">
+        {apiData.map((item, index) => (
+          <Card key={index} data={item} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Page;
