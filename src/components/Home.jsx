24aa614bb55cf4bfac1179bf833151e525131fe6
Nithin <nithinkatkam504106@gmail.com>
2024-04-08 01:58:24 +0530

    flhub

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,17 +1,20 @@
 import Image from 'next/image'
 export default function Home() {
   return (
+
 <div>
   <section className="bg-gradient-to-r from-blue-200 to-blue-500 text-white py-16">
     <div className="container mx-auto text-center">
       <h1 className="text-4xl md:text-5xl font-bold mb-6">
-        Create Courses or upload datasets and take your 100% revenue, zero fees
+        FileHub Marketplace
       </h1>
       <p className="text-lg md:text-xl mb-8">
         Welcome to FileHub <span className="font-bold">FLB</span>, where we are changing the game in providing various courses, datasets, and images.
       </p>
     </div>
   </section>
+
+
 </div>
 
   )
